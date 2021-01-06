54f08233b923a12025ff78559c740166f907ec3e
Dulce Perez <d.perez@kation.com.do>
2021-01-06 17:52:11 -0400

    Adding footer

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,23 @@
   <div id="app">
     <HeaderComponent />
     <router-view />
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HeaderComponent from "@/components/Header.vue";
+import FooterComponent from "@/components/Footer.vue";
 
 export default {
   components: {
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   }
 };
 </script>
 
 <style lang="scss">
 @import "./assets/scss/styles";
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,81 @@
+<template>
+  <footer>
+    <div class="container">
+      <ul class="social-media">
+        <li>
+          <a href="https://www.instagram.com/dulcep.c/" target="blank"
+            ><i class="fab fa-instagram"></i
+          ></a>
+        </li>
+        <li>
+          <a href="#!"><i class="fab fa-linkedin-in"></i></a>
+        </li>
+      </ul>
+      <hr />
+      <p>© 2020 My Website. All rights reserved</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent"
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/styles";
+
+footer {
+  background: #222222;
+  padding: 40px 0px;
+
+  hr {
+    border-color: #454545;
+  }
+
+  p {
+    color: #454545;
+    text-align: center;
+    margin-top: 30px;
+    margin-bottom: 0px;
+  }
+
+  .social-media {
+    padding: 0px;
+    margin: 0px;
+    margin-bottom: 30px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    li {
+      @include size(40px);
+      border-radius: 50%;
+      border: 1px solid #454545;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      margin-right: 15px;
+      transition: 0.3s;
+
+      &:last-child {
+        margin-right: 0px;
+      }
+
+      a {
+        color: #454545;
+        font-size: 16px;
+      }
+
+      &:hover {
+        border-color: rgb(255, 166, 0);
+
+        a {
+          color: rgb(255, 166, 0);
+        }
+      }
+    }
+  }
+}
+</style>
