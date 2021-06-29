6f3a2fb31a1fb3639dab8b9cc2d1767969cf005f
Dulce Perez <dulcefrancelispc@gmail.com>
2021-06-29 15:17:44 -0400

    adding container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,39 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          margin: "0 auto",
+          padding: "0 15px",
+          "@screen sm": {
+            maxWidth: "540px"
+          },
+          "@screen md": {
+            maxWidth: "720px"
+          },
+          "@screen lg": {
+            maxWidth: "960px"
+          },
+          "@screen xl": {
+            maxWidth: "1140px"
+          },
+        },
+        ".container-fluid": {
+          maxWidth: "100%",
+          margin: "0 auto",
+          padding: "0 15px",
+          width: "100%"
+        }
+      })
+    }
+  ],
 }
